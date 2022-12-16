4fdf18d15ec2faeeaccebef16188828b2046c63e
mubarak7785 <mubarakjinabade07@gmail.com>
2022-12-16 16:46:55 +0530

    done with firebase authentication

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,20 @@
-
+import "./home.css"
 import {signInWithGoogle} from "../services/firebase"
 import { Link } from "react-router-dom"
 export const Home=()=>{
     return(
         <div>
-            <h3 onClick={signInWithGoogle}>Sign With Google</h3>
-           <Link to="/next"> <h1>click for profile page</h1></Link>
+            <div className="one">
+            <h1>topmate</h1>
+            </div>
+
+          
+           <button className="button" onClick={signInWithGoogle}>Sign With Google</button>
+           <br />
+           <h4>After the login click below button</h4>
+           <br />
+           <Link to="/next"><button className="button">click for Home page</button></Link>
+        
         
         </div>
     )
--- src/components/next.jsx
+++ src/components/next.jsx
@@ -1,4 +1,11 @@
+
 import "./next.css"
+import { AiOutlineHome ,AiOutlineQuestionCircle,AiOutlineGift} from 'react-icons/Ai'
+import { FiPhoneCall } from 'react-icons/Fi'
+import { MdOutlineEventAvailable,MdMiscellaneousServices } from 'react-icons/Md'
+import { TbBrandGoogleAnalytics } from 'react-icons/Tb'
+
+
 export const Next=()=>{
     return(
         <div>
@@ -12,6 +19,17 @@ export const Next=()=>{
                 </div>  
                 <br />  
                 <button>Publish Profile</button>
+                    <br />
+                    <br />
+                    <br />
+                    <div className="try"><span><AiOutlineHome/></span><p >Home</p></div>
+                    <div className="try"><span><FiPhoneCall/></span><p >Call</p></div>
+                    <div className="try"><span><AiOutlineQuestionCircle/></span><p >Queries</p></div>
+                    <div className="try"><span><MdOutlineEventAvailable/></span><p >Availability</p></div>
+                    <div className="try"><span><MdMiscellaneousServices/></span><p >Services</p></div>
+                    <div className="try"><span><TbBrandGoogleAnalytics/></span><p >Analytics</p></div>
+                    <div className="try"><span><AiOutlineGift/></span><p >Rewards</p></div>
+     
             </div>
          
            
